94794df1d20e0748663919aa95ddeabe4bf2dc47
Shriprajwal <shriprajwal_ctrl@juhackathon.org>
2019-07-17 06:56:30 +0530

    Posting done

--- govern/post/models.py
+++ govern/post/models.py
@@ -1,19 +1,19 @@
 """Models.py file."""
+# Create your models here.
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-
-# Create your models here.
+from django.urls import reverse
 
 
 class Post(models.Model):
-    """Database containing all the posts."""
-
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        """Return string when Post object is queried."""
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
